366f0fc710df5154486e8c9553d2c909939ab94f
snowerdries <dries.nicolay@gmail.com>
2016-02-24 08:30:51 +0100

    first commit

--- test/spec/tink-skeleton-angular.js
+++ test/spec/tink-skeleton-angular.js
@@ -4,6 +4,12 @@ describe('tink-skeleton-angular', function() {
   var bodyEl = $('body'), sandboxEl;
   var $compile, $templateCache, scope;
 
+  /*var templates = {
+    'default': {
+      scope: {},
+      element: ''
+    }
+  };*/
   // beforeEach(module('tink'));
 
   beforeEach(inject(function (_$rootScope_, _$compile_, _$templateCache_) {
@@ -19,22 +25,14 @@ describe('tink-skeleton-angular', function() {
     sandboxEl.remove();
   });
 
-  function compileDirective(template, locals) {
+  /*function compileDirective(template, locals) {
     template = templates[template];
     angular.extend(scope, angular.copy(template.scope || templates['default'].scope), locals);
     var element = $(template.element).appendTo(sandboxEl);
     element = $compile(element)(scope);
     scope.$digest();
     return jQuery(element[0]);
-  }
-
-  var templates = {
-    'default': {
-      scope: {},
-      element: ''
-    }
-  };
-
+  }*/
 
   describe('default', function() {
     it('should run this basic setup',function(){
