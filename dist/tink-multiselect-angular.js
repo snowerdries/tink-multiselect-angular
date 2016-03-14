17a6b84860bb8cba79e4ece85191dfbd972f0817
WillemDN <Willem.de.nys@gmail.com>
2016-03-14 11:37:14 +0100

    removeRedundantAttributes: true to false to fix styling bug

--- dist/tink-multiselect-angular.js
+++ dist/tink-multiselect-angular.js
@@ -103,7 +103,7 @@
   $templateCache.put('templates/multiselect.html',
     "<div class=multiselect> <div class=faux-input data-ng-click=changeEditMode()> <span class=placeholder data-ng-if=noItemsSelected()>{{::emptyText}}</span>\n" +
     "<span class=label-primary data-ng-repeat=\"item in selectedItems()\"> {{item[DisplayProperty]}}\n" +
-    "<button class=upload-btn-delete data-ng-click=\"deselectItem(item); $event.stopPropagation()\"><span class=sr-only>Leegmaken</span></button> </span> </div> <div data-ng-if=\"editMode && notSelectedItems() != ''\" class=popover> <input id=search ng-if=showSearchbar ng-model=search class=popover-search ng-model-options={debounce:333} placeholder=Search> <div class=popover-list> <ul class=popover-list-buttons> <li data-ng-repeat=\"item in ngModel | filter:search track by $index\" data-ng-click=selectItem(item)> <a href=\"\" ng-class=\"item.isChecked ? 'tink-bg-grass' :''\"><span>{{item[DisplayProperty]}}</span></a> </li> </ul> </div> </div> </div>"
+    "<button class=upload-btn-delete data-ng-click=\"deselectItem(item); $event.stopPropagation()\"><span class=sr-only>Leegmaken</span></button> </span> </div> <div data-ng-if=\"editMode && notSelectedItems() != ''\" class=popover> <input id=search type=text ng-if=showSearchbar ng-model=search class=popover-search ng-model-options={debounce:333} placeholder=Search> <div class=popover-list> <ul class=popover-list-buttons> <li data-ng-repeat=\"item in ngModel | filter:search track by $index\" data-ng-click=selectItem(item)> <a href=\"\" ng-class=\"item.isChecked ? 'tink-bg-grass' :''\"><span>{{item[DisplayProperty]}}</span></a> </li> </ul> </div> </div> </div>"
   );
 
 }]);
